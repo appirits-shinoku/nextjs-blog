3d26b800641aa9ee02725ca39aa602a2f389f181
daisuke.shinoku <daisuke.shinoku@appirits.com>
2022-02-03 11:20:50 +0900

    TS化完了

--- lib/posts.ts
+++ lib/posts.ts
@@ -18,11 +18,11 @@ export function getSortedPostsData() {
 
     return {
       id,
-      ...matterResult.data
+      ...(matterResult.data as { date: string; title: string})
     }
   })
   return allPostsData.sort((a, b) => {
-    if (a.data < b.data) {
+    if (a.date < b.date) {
       return 1
     } else {
       return -1
@@ -69,6 +69,6 @@ export async function getPostData(id) {
   return {
     id,
     contentHtml,
-    ...matterResult.data
+    ...(matterResult.data as { date:string; title: string })
   }
 }
